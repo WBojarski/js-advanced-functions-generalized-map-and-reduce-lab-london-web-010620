9de865dd83f125ee8a7b49f9ecabcb7c06f92b9b
Wojciech Bojarski <bojawojt@gmail.com>
2020-02-24 15:45:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+const map = (array, callbackFn) => {
+  let newArr = [];
+
+  array.forEach(element => newArr.push(callbackFn(element)));
+  return newArr;
+};
+
+const reduce = (array, callbackFn, starting = 0) => {
+  if (starting) {
+    return array.reduce(callbackFn, starting);
+  } else {
+    return array.reduce(callbackFn);
+  }
+};
